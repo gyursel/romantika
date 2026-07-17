e6308408cee1813340020f3a20520f2f88bbc10b
gyursel <87885104+gyursel@users.noreply.github.com>
2026-07-17 03:46:09 +0300

    Fix import statement typo in index.js

--- menu-final/pages/index.js
+++ menu-final/pages/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+pimport { useState, useEffect, useRef } from 'react';
 import Head from 'next/head';
 import { db } from '../lib/firebase';
 import { doc, getDoc, setDoc, collection, getDocs, addDoc } from 'firebase/firestore';
